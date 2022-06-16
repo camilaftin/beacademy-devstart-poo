104962c4aa6b9a2d320ceadd42ef89dcc1d42a98
camila <cftinelli@outlook.com>
2022-06-16 16:45:11 -0300

    aula 08

--- modulo02/aula01/Curso.php
+++ modulo02/aula01/Curso.php
@@ -8,4 +8,37 @@ class Curso{
   public int $cargaHoraria;
   public string $descricao;
 
+  public function getNomeCurso():string 
+  {
+    return $this->nomeCurso;
+  }
+
+  public function setNomeCurso(string $novoNomeCurso):void 
+  {
+    
+    $this->nomeCurso = $novoNomeCurso;
+  }
+
+  public function getcargaHoraria():float 
+  {
+    return $this->cargaHoraria;
+  }
+
+  public function setcargaHoraria(float $novoCargaHoraria):void 
+  {
+
+    $this->cargaHoraria = $novoCargaHoraria;
+  }
+
+  public function getDescricao():string 
+  {
+    return $this->descricao;
+  }
+
+  public function setDescricao(string $novoDescricao):void 
+  {
+    
+    $this->descricao = $novoDescricao;
+  }
+
 }
